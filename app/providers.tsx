6f4976eb7e579413cb9b15243eaa735ef8557f8b
Mohick <haokietks7@gmail.com>
2026-05-18 15:36:51 +0700

    update standart source

---
+++ app/providers.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState, ReactNode } from "react";
+
+export default function Providers({ children }: { children: ReactNode }) {
+  const [queyryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queyryClient}>{children}</QueryClientProvider>
+  );
+}
