21184d48dd7cf0f85bf6e964af28d7e0830b5d5a
Mohick <haokietks7@gmail.com>
2026-05-27 10:23:38 +0700

    fix:upload cấu hình

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  /* các cấu hình khác của ông nếu có giữ nguyên */
 };
 
 export default nextConfig;
