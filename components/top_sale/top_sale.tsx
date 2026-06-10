df63c765fccc3a19a8401f816644096b2b1e54c1
tranthonghoang06 <tranthonghoangt1@gmail.com>
2026-06-10 13:56:19 +0700

    fix: change homepage top sale products to 2 columns on mobile

--- components/top_sale/top_sale.tsx
+++ components/top_sale/top_sale.tsx
@@ -98,7 +98,7 @@ const TopSale = () => {
                 </div>
 
                 {/* Products Grid */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8">
+                <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-8">
                     {topSaleProducts.map((product) => {
                         const isHovered = hoveredCard === product.id;
                         return (
